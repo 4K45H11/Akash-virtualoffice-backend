9547ec8b53565d5873471ff7a3969bed3e7c99a8
4K45H11 <akshazrajana@gmail.com>
2025-07-30 20:35:03 +0530

    initial setup done

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config()
+
+const authRoutes = require('./routes/auth.router');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/',(req,res)=>{
+  res.status(200).json({message:'Welcome to the virtual office server'})
+})
+
+app.use('/api/auth', authRoutes);
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+  console.log('MongoDB Connected');
+  app.listen(5000, () => console.log('Server running on port 5000'));
+})
+.catch(err => console.log(err));
