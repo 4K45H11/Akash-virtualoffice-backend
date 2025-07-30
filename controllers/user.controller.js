49658adaf8001eff0471b690e1e90046c7430c84
4K45H11 <akshazrajana@gmail.com>
2025-07-30 18:42:39 +0530

    initial commit

--- controllers/user.controller.js
+++
@@ -1,9 +0,0 @@
-exports.userDashBoard = async (req, res) => {
-    try {
-        const user = req.user;
-        res.status(200).json(user)
-    } catch (error) {
-        res.status(500).json({err:error.message})
-    }
-}
-
