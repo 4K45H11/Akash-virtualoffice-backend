bb2461a1216e77faddf6c7e7f67a0099e54db893
4K45H11 <akshazrajana@gmail.com>
2025-08-06 13:26:59 +0530

    features added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -30,7 +30,7 @@ exports.login = async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
 
-        const token = jwt.sign({ id: user._id, role: user.role }, 'secretKey', { expiresIn: '1d' });
+        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_KEY, { expiresIn: '1d' });
         res.json({ token, user: { name: user.name, role: user.role } });
     } catch (err) {
         res.status(500).json({ msg: err.message });
--- backend/middleware/auth.middle.js
+++ backend/middleware/auth.middle.js
@@ -3,14 +3,14 @@ const jwt = require('jsonwebtoken');
 const verifyToken = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(401).json({ msg: "no token provided" });
-  console.log(token)
+  // console.log(token)
   try {
     const decoded = jwt.verify(token, process.env.JWT_KEY);
     req.user = decoded;
     next();
   } catch (err) {
     
-    return res.status(403).json({ msg: "invalid token" });
+    return res.status(403).json({ msg: err.message });
   }
 };
 
